cb5b2753086462f8d8c869924e97c9385691eddd
Guilherme Fischer <guilherme@gfischer.com.br>
2022-12-22 08:53:12 -0300

    fix ( objects ): Remove scrolling on mmf object

--- src/renderer/MMFEmbedded/index.js
+++ src/renderer/MMFEmbedded/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './styles.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "./styles.css";
 
 const MMFEmbed = ({ block, contentState }) => {
   const entity = contentState.getEntity(block.getEntityAt(0));
   const { src, height, width } = entity.getData();
 
-
-  return (<div className="MMFfluidMedia">
-            <iframe height={height} width={width} src={src} frameBorder="0" allowFullScreen title="Wysiwyg Embedded Content" />
-          </div>);
+  return (
+    <div className="MMFfluidMedia">
+      <iframe height={height} width={width} src={src} frameBorder="0" allowFullScreen title="Wysiwyg Embedded Content" scrolling="no" />
+    </div>
+  );
 };
 
 MMFEmbed.propTypes = {
